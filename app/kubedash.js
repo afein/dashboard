85c48fa2c781239c6039eae96f2024029aa174e1
Alex Mavrogiannis <almavrog@google.com>
2015-07-16 16:53:00 -0700

    cleaning up timers, node view operational

--- app/kubedash.js
+++ app/kubedash.js
@@ -23,6 +23,10 @@
         // route for the Cluster page
         .when('/', {
           templateUrl : 'pages/cluster.html',
+        })
+        // route for the Node page
+        .when('/node', {
+          templateUrl : 'pages/node.html',
         });
   }]);
 
@@ -76,22 +80,26 @@
         $scope.data.push({key: 'CPU Utilization', area: true, values:[]});
         $scope.run = true;
 
-        var memUsage = '/api/v1/model/cluster/memory-usage?start=';
-        var memLimit = '/api/v1/model/cluster/memory-limit?start=';
-        var cpuUsage = '/api/v1/model/cluster/cpu-usage?start=';
-        var cpuLimit = '/api/v1/model/cluster/cpu-limit?start=';
+        var memUsage = 'api/v1/model/cluster/memory-usage?start=';
+        var memLimit = 'api/v1/model/cluster/memory-limit?start=';
+        var cpuUsage = 'api/v1/model/cluster/cpu-usage?start=';
+        var cpuLimit = 'api/v1/model/cluster/cpu-limit?start=';
 
-            // scope.poll appends memory and cpu utilization to the chart
-            $scope.poll = function() {
-              pollUtilization(memUsage, memLimit, $scope, 0, $http, $q);
-              pollUtilization(cpuUsage, cpuLimit, $scope, 1, $http, $q);
-            };
+        // scope.poll appends memory and cpu utilization to the chart
+        $scope.poll = function() {
+          pollUtilization(memUsage, memLimit, $scope, 0, $http, $q);
+          pollUtilization(cpuUsage, cpuLimit, $scope, 1, $http, $q);
+        };
 
         // Poll for new data every 5 seconds
-        $interval($scope.poll, 5000);
+        $scope.pollPromise = $interval($scope.poll, 10000);
 
         // Trigger the first poll as soon as content is loaded
         $scope.$watch('$viewContentLoaded', $scope.poll);
+
+        $scope.$on('$destroy', function () {
+               $interval.cancel($scope.pollPromise);
+        });
 
       }]);
 
@@ -104,7 +112,7 @@
     console.log("poll");
     $q.all([
         // Get Metric Usage and store in the time-descending usage array .
-        $http.get(window.location + usageLink + $scope.stamp)
+        $http.get(usageLink + $scope.stamp)
         .success(function(data) {
           if ((data.metrics == undefined) || (data.metrics.length == 0)) {
             // No metrics are available, postpone
@@ -120,7 +128,7 @@
 
         }),
         // Get Metric Usage and store in the time-descending usage array .
-        $http.get(window.location + limitLink + $scope.stamp)
+        $http.get(limitLink + $scope.stamp)
         .success(function(data) {
           if ((data.metrics == undefined) || (data.metrics.length == 0)) {
             // No metrics are available, postpone
@@ -150,4 +158,77 @@
           }
         })
   };
+
+  app.controller('nodeUtil', ['$scope', '$http', '$interval', '$q',
+      function($scope, $http, $interval, $q) {
+        $scope.options = {
+          chart: {
+            type: 'lineWithFocusChart',
+            height: 400,
+            margin : {
+              top: 20,
+              right: 20,
+              bottom: 60,
+              left: 40
+            },
+            transitionDuration: 500,
+            useInteractiveGuideline: true,
+            lines: {
+              forceY: [0, 1],
+            },
+            color: ['#31708f', '#a94442'],  // light blue, light red
+            xAxis: {
+              axisLabel: 'Time',
+              tickFormat: function(d) {
+                return d3.time.format('%X')(new Date(d));
+              },
+              staggerLabels:true
+            },
+            x2Axis: {
+              tickFormat: function(d) {
+                return d3.time.format('%X')(new Date(d));
+              }
+            },
+            yAxis: {
+              axisLabel: 'Utilization',
+              tickFormat: function(d) {
+                return d3.format('%')(d3.round(d, 3));
+              }
+            },
+            y2Axis: {
+              tickFormat: function(d) {
+                return d3.format('%')(d3.round(d, 3));
+              }
+            }
+
+          }
+        };
+
+        $scope.stamp = (new Date(0)).toISOString();
+        $scope.data = [{key: 'Memory Utilization', area: true, values:[]}];
+        $scope.data.push({key: 'CPU Utilization', area: true, values:[]});
+        $scope.run = true;
+
+        var memUsage = 'api/v1/model/cluster/memory-usage?start=';
+        var memLimit = 'api/v1/model/cluster/memory-limit?start=';
+        var cpuUsage = 'api/v1/model/cluster/cpu-usage?start=';
+        var cpuLimit = 'api/v1/model/cluster/cpu-limit?start=';
+
+        // scope.poll appends memory and cpu utilization to the chart
+        $scope.poll = function() {
+          pollUtilization(memUsage, memLimit, $scope, 0, $http, $q);
+          pollUtilization(cpuUsage, cpuLimit, $scope, 1, $http, $q);
+        };
+
+        // Poll for new data every 5 seconds
+        $scope.pollPromise = $interval($scope.poll, 10000);
+
+        // Trigger the first poll as soon as content is loaded
+        $scope.$watch('$viewContentLoaded', $scope.poll);
+
+        $scope.$on('$destroy', function () {
+               $interval.cancel($scope.pollPromise);
+        });
+
+    }]);
 })();
